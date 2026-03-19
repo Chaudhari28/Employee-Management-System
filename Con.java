b1849eb71cc2a9f64abb013a36f11503ee6b5bd8
Rucha Chaudhari <147044628+Chaudhari28@users.noreply.github.com>
2026-03-19 23:25:45 +0530

    Add files via upload

---
+++ Con.java
@@ -0,0 +1,24 @@
+package Emplyopee.Management.System;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Con {
+
+    Connection connection;
+    Statement statement;   //
+
+
+    public Con(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/EmployeeManagement","root","rucha");
+            statement = connection.createStatement();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
